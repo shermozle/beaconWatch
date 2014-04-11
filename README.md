51f2cf4b482a9504843b4709cd48f63913e26a17
Simon Rumble <simon@simonrumble.com>
2014-04-11 14:58:28 +1000

    Ready for first prototype to be unleashed on the world

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 beaconWatch
 ===========
 
-Use node.js to load web pages and parse out the analytics beacons of interest
+Use node.js to load web pages and parse out the analytics beacons of interest.
+
+What is does is open a browser via Selenium, spin up a proxy server and tell
+Selenium to use it, then browse to your URL. The proxy parses out the URLs of
+beacons according to some basic rules.
+
+Currently just a proof of concept. Aim is to have something that will drive
+Saucelabs browsers to check in all browsers and to be build into some kind of
+unit test framework. Also doesn't handle HTTPS so the Facebook beacons don't
+work and it won't work with secure sites.
+
+Requires these npm modules:
+* httpProxy
+* url
+* colors
+* selenium-webdriver
+* optimist
+
+You also need to have a locally running version of the Selenium server like:
+http://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar
+
+Run it with:
+java -jar selenium-server-standalone-2.41.0.jar
---
+++ beaconWatch.js
@@ -0,0 +1,123 @@
+#!/usr/bin/env node
+
+/*
+	TODO
+	* Write tests for existence of particular beacons
+	* Interface somewhere so you can automate this shit
+	* Use Saucelabs instead of local Selenium
+	* GTM test mode
+*/
+
+var http = require('http'),
+		httpProxy = require('http-proxy'),
+		url = require('url'),
+		fs = require('fs'),
+		colors = require('colors'),
+		proxyPort = Math.floor(Math.random()*(12000-8000+1)+8000),
+		webdriver = require('selenium-webdriver'),
+		proxy = require('selenium-webdriver/proxy'),
+		beacons = {},
+		interestingRequest = {},
+		argv = require('optimist')
+			.usage('Test the analytics beacons in a web page.\nUsage: $0')
+			.demand('u')
+			.alias('u', 'url')
+			.describe('u', 'URL to test')
+			.alias('r', 'require')
+			.describe('r', 'Required analytics tools. One or more of: "omniture", "googleanalytics", "nielsen" separated by commas.')
+			.alias('d', 'debug')
+			.describe('d','Enable debugging output to console.')
+			.argv;
+
+/*
+	Proxy server that pushes any requests for s_code.js to run through the server defined above.
+	Looks at all requests and logs any analytics beacon requests to the console.
+*/
+var proxyServer = httpProxy.createProxy();
+
+var outboundServer = require('http').createServer(function(req, res) {
+	if (argv.debug) {
+		console.log('Proxy: '.red.bold + req.url);
+	}
+	isThisInteresting(req);
+	proxyServer.web(req, res, {
+		target: req.url
+	}, function(e) {
+		console.log('Proxy error: '.red.bold + req.url);
+		console.log(e);
+	});
+}).listen(proxyPort);
+
+var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.firefox())
+		.setProxy(proxy.manual({
+			http: 'localhost:' + proxyPort
+		}))
+    .build();
+
+driver.manage().timeouts().pageLoadTimeout(60000);
+driver.get(argv.url).then(function() {
+	driver.getTitle().then(function(title) {
+		console.log('Page title: '.blue.bold + title);
+	});
+}).then(function() {
+	console.log(beacons);
+}).then(function() {
+	driver.quit();
+}).then(function() {
+	outboundServer.close();
+});
+
+var isThisInteresting = function (request) {
+	var beacon = testForBeacons(request);
+	if (beacon) {
+		beacons[beacon.type] = beacons[beacon.type] || [];
+		beacons[beacon.type].push(beacon.url);
+	} else {
+		return false;
+	}
+};
+
+var testForBeacons = function (request) {
+	var parsedRequest = url.parse(request.url);
+	if (parsedRequest.pathname.match('/b/ss') !== null) {
+		return({
+			type: 'Omniture',
+			url: request.url
+		});
+	} else
+	if (parsedRequest.host.match('adobetag.com') !== null) {
+		return({
+			type: 'Adobe Tag Container',
+			url: request.url
+		});
+	} else
+	if (request.url.match('secure-au.imrworldwide.com/cgi-bin/m') !== null) {
+		return({
+			type: 'Nielsen',
+			url: request.url
+		});
+	} else
+	if (request.url.match('__utm.gif') !== null) {
+		return({
+			type: 'Google Analytics',
+			url: request.url
+		});
+	}
+	if (parsedRequest.host.match('chartbeat.com') !== null) {
+		return({
+			type: 'Chartbeat',
+			url: request.url
+		});
+	}
+	if (request.url.match('www.facebook.com/connect') !== null) {
+		return({
+			type: 'Facebook',
+			url: request.url
+		});
+	}
+
+	else {
+		return(false);
+	}
+};
